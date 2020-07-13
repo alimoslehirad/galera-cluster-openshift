0779b495d5eb81dc131c831032c7aebaa7c6e864
root <alimoslehirad@gmail.com>
2020-07-13 13:52:54 +0430

    readiness prob change

--- k8s-mariadb-galera-centos/root/usr/share/container-scripts/mysql/readiness-probe.sh
+++ k8s-mariadb-galera-centos/root/usr/share/container-scripts/mysql/readiness-probe.sh
@@ -8,7 +8,8 @@ MYSQL_USER="root"
 MYSQL_PASS="xHp9x4gNejfQ4gDf"
 MYSQL_HOST="localhost"
 
-mysql --protocol=socket --socket=/var/run/mysql/mysql.sock -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} -e"SHOW DATABASES;"
+#mysql --protocol=socket --socket=/var/run/mysql/mysql.sock -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} -e"SHOW DATABASES;"
+mysql -e"SHOW DATABASES;"
 
 if [ $? -ne 0 ]; then
   exit 1
