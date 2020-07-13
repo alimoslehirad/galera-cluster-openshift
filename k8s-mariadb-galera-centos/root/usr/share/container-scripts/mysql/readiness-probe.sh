1bea0ea62bac88a35531ed419bf0ecedbbd066ac
root <alimoslehirad@gmail.com>
2020-07-13 12:17:56 +0430

    readinessprob edited

--- k8s-mariadb-galera-centos/root/usr/share/container-scripts/mysql/readiness-probe.sh
+++ k8s-mariadb-galera-centos/root/usr/share/container-scripts/mysql/readiness-probe.sh
@@ -4,8 +4,8 @@
 # openshift-mariadb-galera: mysqld readinessProbe
 #
 
-MYSQL_USER="readinessProbe"
-MYSQL_PASS="readinessProbe"
+MYSQL_USER="root"
+MYSQL_PASS="xHp9x4gNejfQ4gDf"
 MYSQL_HOST="localhost"
 
 mysql --protocol=socket --socket=/var/run/mysql/mysql.sock -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} -e"SHOW DATABASES;"
